b032013ea9266cc7f66dbf256ca17681511c6aba
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-08-27 16:59:46 +0100

    social login logic

--- auth.ts
+++ auth.ts
@@ -32,4 +32,23 @@ export const auth = betterAuth({
       },
     },
   },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+
+   account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["google", "github"],
+    },
+  },
 });
