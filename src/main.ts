4133630adbf72f3e5c254574fb119b4ee2720e4d
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-08-27 22:35:27 +0100

    testing social in main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log("🔥🔥🔥 THIS IS MY NEW CODE 🔥🔥🔥");
+  
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
@@ -13,5 +15,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3002);
 }
+
 
 bootstrap();
